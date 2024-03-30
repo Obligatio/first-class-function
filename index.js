6dc897781b2acf38a3908e7b7606493ba645d499
kevin <kevinke386@gmail.com>
2024-03-30 23:54:42 +0300

    first class

---
+++ index.js
@@ -0,0 +1,20 @@
+const  receiveAFunction = (spy) => spy()
+
+
+
+function returnsANamedFunction(){
+    return function receivesAFunction(spy){
+        return 'spy'
+    }
+
+}
+
+
+
+function returnAnAnonymousFunction(){
+    return function(name){
+        return 'name'
+    }
+}
+
+
